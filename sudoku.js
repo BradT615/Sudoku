9a4c1b746c9e9d095bac05eaf29469f995ae15df
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-06-15 17:32:23 -0400

    Finished js

--- sudoku.js
+++ sudoku.js
@@ -1,53 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let puzzle;
+
   // Sudoku puzzle generation function
   function generateSudoku() {
-    const puzzle = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
-  
-    function isValid(num, row, col) {
-      for (let i = 0; i < 9; i++) {
-        if (puzzle[row][i] === num || puzzle[i][col] === num) {
-          return false;
-        }
-      }
-  
-      const startRow = Math.floor(row / 3) * 3;
-      const startCol = Math.floor(col / 3) * 3;
-      for (let i = startRow; i < startRow + 3; i++) {
-        for (let j = startCol; j < startCol + 3; j++) {
-          if (puzzle[i][j] === num) {
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-  
-    function solveSudoku() {
-      for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-          if (puzzle[row][col] === 0) {
-            for (let num = 1; num <= 9; num++) {
-              if (isValid(num, row, col)) {
-                puzzle[row][col] = num;
-                if (solveSudoku()) {
-                  return true;
-                }
-                puzzle[row][col] = 0;
-              }
-            }
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-  
-    // Generate a fully solved puzzle
-    solveSudoku();
-  
+    puzzle = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
+    solveSudoku(puzzle);
     // Remove numbers
     let count = 0;
-    while (count < 64) {
+    while (count < 70) {
       const row = Math.floor(Math.random() * 9);
       const col = Math.floor(Math.random() * 9);
       if (puzzle[row][col] !== 0) {
@@ -55,11 +15,61 @@ document.addEventListener("DOMContentLoaded", function() {
         count++;
       }
     }
-    return puzzle;
+  }
+
+  function isValid(num, row, col) {
+    for (let i = 0; i < 9; i++) {
+      if (puzzle[row][i] === num || puzzle[i][col] === num) {
+        return false;
+      }
+    }
+  
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+      for (let j = startCol; j < startCol + 3; j++) {
+        if (puzzle[i][j] === num) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function isFilled(grid) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (grid[row][col] === 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // Generate a fully solved puzzle
+  function solveSudoku(puzzle) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (puzzle[row][col] === 0) {
+          for (let num = 1; num <= 9; num++) {
+            if (isValid(num, row, col)) {
+              puzzle[row][col] = num;
+              if (solveSudoku(puzzle)) {
+                return true;
+              }
+              puzzle[row][col] = 0;
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   function generate() {
-    const puzzle = generateSudoku();
+    generateSudoku();
   
     const box = document.querySelector('.box');
     box.innerHTML = '';
@@ -73,27 +83,116 @@ document.addEventListener("DOMContentLoaded", function() {
         const cellValue = puzzle[row][col];
         cell.textContent = cellValue !== 0 ? cellValue : '';
         if (cellValue !== 0) {
-          cell.dataset.locked = true; // Add a "locked" attribute to locked cells
+          cell.dataset.locked = true;
         }
         box.appendChild(cell);
       }
     }
+    addBorders();
   }
 
   generate();
 
+  // Event listener for numpad button clicks
+  const numButtons = document.querySelectorAll('.num-btn');
+  numButtons.forEach((button) => {
+    button.addEventListener('click', function(event) {
+      const clickedButton = event.target;
+      const selectedCell = document.querySelector('.selected');
+  
+      if (selectedCell && !selectedCell.hasAttribute('data-locked')) {
+        const selectedValue = clickedButton.textContent;
+        const row = parseInt(selectedCell.dataset.row);
+        const col = parseInt(selectedCell.dataset.col);
+  
+        // Update the cell first
+        selectedCell.textContent = selectedValue;
+        if (selectedValue !== '') {
+          selectedCell.classList.add('filled');
+        } else {
+          selectedCell.classList.remove('filled');
+        }
+  
+        // Then perform the checks
+        if (selectedValue !== '' && !isValid(parseInt(selectedValue), row, col)) {
+          selectedCell.style.color = '#e55c6c'; // Invalid move
+        } else {
+          // Copy the current puzzle state
+          const tempPuzzle = JSON.parse(JSON.stringify(puzzle));
+          tempPuzzle[row][col] = parseInt(selectedValue);
+  
+          // Check if all cells are filled
+          if (isFilled(tempPuzzle)) {
+            selectedCell.style.color = ''; // Reset to default color
+            
+            const box = document.querySelector('.box');
+            box.dataset.filled = true;
+          } else if (!solveSudoku(tempPuzzle)) {
+            selectedCell.style.color = '#e55c6c'; // Unsolvable move
+          } else {
+            // If the move is valid and the puzzle is solvable, update the actual puzzle
+            puzzle[row][col] = parseInt(selectedValue);
+            selectedCell.style.color = ''; // Reset to default color
+          }
+        }
+      }
+    });
+  });
+  
+  
 
 
 
 
 
 
+  // Event listener for clear button
+  const clearButton = document.querySelector('.clear-btn');
+  clearButton.addEventListener('click', function() {
+    const selectedCell = document.querySelector('.selected');
 
+    if (selectedCell && !selectedCell.hasAttribute('data-locked')) {
+      selectedCell.textContent = '';
+      selectedCell.classList.remove('filled');
+    }
+  });
 
+  const solveButton = document.querySelector('.solve-btn');
+  solveButton.addEventListener('click', function() {
+    // action to perform when the button is clicked
+    // for instance, solve the current puzzle and update the board
+    const solved = solveSudoku(puzzle);
 
+    // If the puzzle is solved successfully, update the board
+    if (solved) {
+      const box = document.querySelector('.box');
+      box.innerHTML = '';
 
+      for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+          const cell = document.createElement('div');
+          cell.classList.add('cell');
+          cell.dataset.row = row;
+          cell.dataset.col = col;
+          const cellValue = puzzle[row][col];
+          cell.textContent = cellValue !== 0 ? cellValue : '';
+          if (cellValue !== 0) {
+            cell.dataset.locked = true;
+          }
+          box.appendChild(cell);
+        }
+      }
+      addBorders();
+    }
+  });
 
-
+  // Event listener for new game button
+  const newGameButton = document.querySelector('.new-game-btn');
+  newGameButton.addEventListener('click', function() {
+    const box = document.querySelector('.box');
+    box.removeAttribute('data-filled');
+    generate();
+  });
 
   // Event listener for cell selection
   const box = document.querySelector('.box');
@@ -105,41 +204,43 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Add border
-  const cells = document.querySelectorAll('.cell');
-  for (let i = 0; i < 9; i++) {
-    for (let j of [2, 5]) {
-      cells[i * 9 + j].style.borderRight = '6px solid #344861';
-    }
-    for (let j of [2, 5]) {
-      cells[j * 9 + i].style.borderBottom = '6px solid #344861';
+  function addBorders() {
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < 9; i++) {
+      for (let j of [2, 5]) {
+        cells[i * 9 + j].style.borderRight = '6px solid #344861';
+      }
+      for (let j of [2, 5]) {
+        cells[j * 9 + i].style.borderBottom = '6px solid #344861';
+      }
     }
   }
-  
+
   // Function to highlight related cells
   function selectCell(cell) {
     const selectedCells = document.querySelectorAll('.selected, .selected-main, .selected-related, .same-value');
     selectedCells.forEach((selectedCell) => {
       selectedCell.classList.remove('selected', 'selected-main', 'selected-related', 'same-value');
     });
-  
+
     cell.classList.add('selected');
     cell.classList.add('selected-main');
-  
+
     const selectedValue = cell.textContent;
-  
+
     const allCells = document.querySelectorAll('.cell');
     allCells.forEach((boardCell) => {
       const row = parseInt(boardCell.dataset.row);
       const col = parseInt(boardCell.dataset.col);
-  
+
       if (boardCell.textContent === selectedValue && selectedValue !== '') {
         boardCell.classList.add('same-value');
       }
-  
+
       if (row === parseInt(cell.dataset.row) || col === parseInt(cell.dataset.col)) {
         boardCell.classList.add('selected-related');
       }
-  
+
       if (
         Math.floor(row / 3) === Math.floor(parseInt(cell.dataset.row) / 3) &&
         Math.floor(col / 3) === Math.floor(parseInt(cell.dataset.col) / 3)
@@ -147,5 +248,11 @@ document.addEventListener("DOMContentLoaded", function() {
         boardCell.classList.add('selected-related');
       }
     });
+  }
+
+  // Function to check if the puzzle is solvable
+  function isSolvable() {
+    const puzzleCopy = JSON.parse(JSON.stringify(puzzle));
+    return solveSudoku(puzzleCopy);
   }
 });
